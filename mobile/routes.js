8f0c387040daba859cd64c3b3239934cd1fb2dab
Alexandre <alexandre.trapp@hotmail.com>
2020-04-12 19:09:45 -0300

    change .git / app mobile

---
+++ mobile/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+export default function Routes() {
+
+const a = 1;
+    return (
+
+        <NavigationContainer>
+
+            <AppStack.Navigator>
+                <AppStack.Screen name='Incidents' component={Incidents} />
+                <AppStack.Screen name='Detail' component={Detail} />
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    );
+}
